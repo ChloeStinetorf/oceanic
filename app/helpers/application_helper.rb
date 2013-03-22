654db52b5f87f99569032feda1c908cccd6485a4
Chloe <chloe.stinetorf@gmail.com>
2013-03-22 13:26:14 -0400

    session tests passing!

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,18 +3,20 @@ module ApplicationHelper
     nav = ""
 
     if @auth.present? && @auth.is_admin?
-      nav += "<li>#{link_to('Planes', planes_path, :class => 'button tiny')}<li>"
-      nav += "<li>#{link_to('Flights', flights_path, :class => 'button tiny')}<li>"
+      nav += "<li>#{link_to('Planes', '#', :class => 'button radius alert')}<li>"
+      nav += "<li>#{link_to('Flights', '#', :class => 'button radius alert')}<li>"
     end
     if @auth.present? && !@auth.is_admin?
-       nav += "<li>#{link_to('Search', search_path, :class => 'button tiny')}<li>"
-     end
+       nav += "<li>#{link_to('Search', '#', :class => 'button radius alert')}<li>"
+    end
 
     if @auth.present?
-      nav += "<li>#{link_to(@auth.email, '/login', :method => :delete, :remote => true, :class => 'button tiny alert')}<li>"
+      nav += "<li>#{link_to(@auth.email, '/login', :method => :delete, :remote => true, :class => 'button radius alert')}<li>"
     else
-      nav += "<li>#{link_to('Register', new_user_path)}</li>"
-      nav += "<li>#{link_to('Login', login_path)}</li>"
+      nav += "<li>#{link_to('Register', new_user_path, :remote => true, :class => 'button radius alert')}</li>"
+      nav += "<li>#{link_to('Login', login_path, :remote => true, :class => 'button radius alert')}</li>"
     end
   end
 end
+
+
