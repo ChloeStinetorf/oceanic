391c8bbb1fcfe83bb07fc7ea910618e69631626c
Chloe <chloe.stinetorf@gmail.com>
2013-03-22 14:32:56 -0400

    user or admin can login -complete

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -17,9 +17,9 @@ describe 'Users' do
       fill_in('Email', :with => 'bob@gmail.com')
       fill_in('user_password', :with => 'a')
       fill_in('user_password_confirmation', :with => 'a')
-      click_button('Create User')
-      page.should_not have_button('Create User')
-      expect.User.first.name.to eq 'Bob'
+      click_button('Get Lost')
+      page.should_not have_button('Get Lost')
+      expect(User.first.name).to eq 'Bob'
     end
   end
 
@@ -29,7 +29,7 @@ describe 'Users' do
       visit root_path
       click_link('Register')
       click_link('Cancel')
-      page.should_not have_button('Register')
+      page.should_not have_button('Get Lost')
     end
   end
 end
