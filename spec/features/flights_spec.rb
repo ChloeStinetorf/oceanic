5baefb7711786c70bce72450c14445b6916d8375
Jane Sternbach <janesternbach@gmail.com>
2013-03-22 19:13:47 -0400

    tests passing

--- spec/features/flights_spec.rb
+++ spec/features/flights_spec.rb
@@ -1,19 +1,27 @@
 require "spec_helper"
+require 'features/shared/login_helper'
+include LoginHelper
 
 describe "Flights" do
   describe "GET /flights as admin" do
-    it "should have add flight button" do
+    it "should have add flight button", :js => true do
+      admin = FactoryGirl.create(:admin)
       visit flights_path
+      login_to_system(admin)
       page.should have_link("Add Flight")
     end
     it "should have a flight form", :js => true do
+      admin = FactoryGirl.create(:admin)
       visit flights_path
+      login_to_system(admin)
       click_link("Add Flight")
       page.should have_link("Cancel")
       page.should have_button("Create Flight")
     end
     it "should cancel the flight form", :js => true do
+      admin = FactoryGirl.create(:admin)
       visit flights_path
+      login_to_system(admin)
       click_link("Add Flight")
       click_link("Cancel")
       page.should_not have_button("Create Flight")
@@ -23,7 +31,9 @@ describe "Flights" do
   describe "POST /flights as admin" do
     it "should create a flight and add it to the list of flights", :js => true do
       FactoryGirl.create(:plane)
+      admin = FactoryGirl.create(:admin)
       visit flights_path
+      login_to_system(admin)
       click_link("Add Flight")
       fill_in("Name", :with => "A 787")
       fill_in("Origin", :with => "SYD")
