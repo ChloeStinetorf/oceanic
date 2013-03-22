e3ab1fe5aae8fe217ea8b09d1302d49478a9c80a
Jane Sternbach <janesternbach@gmail.com>
2013-03-22 14:19:36 -0400

    flights tests

---
+++ spec/features/flights_spec.rb
@@ -0,0 +1,38 @@
+require "spec_helper"
+
+describe "Flights" do
+  describe "GET /flights as admin" do
+    it "should have add flight button" do
+      visit flights_path
+      page.should have_link("Add Flight")
+    end
+    it "should have a flight form" do
+      visit flights_path
+      click_link("Add Flight")
+      page.should have_link("Cancel")
+      page.should have_button("Create Flight")
+    end
+    it "should cancel the flight form" do
+      visit flights_path
+      click_link("Add Flight")
+      click_link("Cancel")
+      page.should_not have_button("Create Flight")
+    end
+  end
+
+  describe "POST /flights as admin" do
+    it "should create a flight and add it to the list of flights" do
+      visit flights
+      click_link("Add Flight")
+      fill_in("Name", :with => "A 787")
+      fill_in("Origin", :with => "SYD")
+      fill_in("Desitination", :with => "LAX")
+      click_button("Create Flight")
+      page.should_not have_button("Create Flight")
+      expect(Flight.first.name).to eq "A 787"
+      page.should have_text("A 787")
+      page.should have_text("SYD")
+      page.should have_text("LAX")
+    end
+  end
+end
