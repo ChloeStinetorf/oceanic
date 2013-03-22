ee44976c758a14a1ce76f1a2c39311c0c042c989
Alex Fogg <me@alexfogg.com>
2013-03-22 14:06:31 -0400

    register and cancel tests

--- spec/features/sesssion_spec.rb
+++ spec/features/sesssion_spec.rb
@@ -32,13 +32,40 @@ describe 'Session' do
       page.should_not have_link('Login')
     end
 
-  it 'does not log the user into the system if credentials are incorrect', :js => true do
+    it 'does not log the user into the system if credentials are incorrect', :js => true do
+        visit root_path
+        click_link('Login')
+        fill_in('Email', :with => 'generic@gmail.com')
+        fill_in('Password', :with => 'b')
+        click_link('Login')
+        page.should have_link('Login')
+      end
+    end
+
+  describe 'DELETE /login' do
+    it 'logs the user off the system', :js => true do
+      user = FactoryGirl.create(:user)
+      login_to_system(user)
+      click_link(user.email)
+      page.should_not have_link(user.email)
+      page.should have_link('Register')
+      page.should_not have_link('Planes')
+      page.should_not have_link('Search')
+      page.should_not have_button('Get Lost')
+      visit root_path
+      page.should_not have_link(user.email)
+      page.should have_link('Login')
+      page.should_not have_link('Flights')
+      page.should_not have_link('Search')
+    end
+  end
+
+  describe 'JS cancel_login_form()' do
+    it 'removes the login form', :js => true do
       visit root_path
       click_link('Login')
-      fill_in('Email', :with => 'generic@gmail.com')
-      fill_in('Password', :with => 'b')
-      click_link('Login')
-      page.should have_link('Login')
+      click_link('Cancel')
+      page.should_not have_button('Get Lost')
     end
   end
 end
