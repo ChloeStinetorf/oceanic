3a64be892c7a87e551cb2790567b31e85de844ce
Jane Sternbach <janesternbach@gmail.com>
2013-03-24 13:40:36 -0400

    changed login

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,9 +13,11 @@ module ApplicationHelper
       nav += "<li>#{link_to(@auth.email, '/login', :method => :delete, :remote => true, :class => 'button radius alert')}<li>"
     else
       nav += "<li>#{link_to('Register', new_user_path, :remote => true, :class => 'button radius alert')}</li>"
-      nav += "<li>#{link_to('Login', login_path, :remote => true, :class => 'button radius alert')}</li>"
+      nav += "<li><a href='/login' data-dropdown='drop1' class='button radius alert' data-remote='true'>Login</a></li>
+              <ul id='drop1' class='f-dropdown content' data-dropdown-content>
+                <li id='form'>#{ render :partial => 'session/form'}</li>
+              </ul>"
     end
   end
 end
-
 
